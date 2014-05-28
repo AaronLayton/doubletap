9e7a93a656c1c88c3e83ed8d2cfde7d58c2e925b
vsync <vsync.design@gmail.com>
2014-05-28 15:20:09 +0300

    added detection when user is touching and scrolling at the same time, to prevent the events from firing

--- jquery.tap.js
+++ jquery.tap.js
@@ -1,7 +1,7 @@
 /*!
     tap / double tap special event for jQuery
     v 1.0.0
-    (c) 2014 Yair Even-Or <http://dropthebit.com>
+    (c) 2014 Yair Even Or <http://dropthebit.com>
     MIT-style license.
 */
 
@@ -9,22 +9,27 @@
     "use strict";
 
     var tapTimer,
+        moved = false,   // flag to know if the finger had moved while touched the device
         wait = 250; // ms
 
     $.event.special.tap = {
         setup: function(data, namespaces) {
-            var elem = this,
-                $elem = $(elem);
-            $elem.bind('touchend.tap', $.event.special.tap.handler);  // touchend
+            $(this).bind('touchend.tap', $.event.special.tap.handler)
+                   .bind('touchmove.tap', function(){
+                        moved = true;
+                   });
         },
 
         teardown: function(namespaces) {
-            var elem = this,
-                $elem = $(elem);
-            $elem.unbind('touchend.tap');
+            $(this).unbind('touchend.tap touchmove.tap');
         },
 
         handler: function(event){
+            if( moved ){
+                moved = false;
+                return false;
+            }
+
             var elem      = event.target,
                 $elem     = $(elem),
                 lastTouch = $elem.data('lastTouch') || 0,
@@ -32,7 +37,7 @@
                 delta     = now - lastTouch,
                 args      = [].slice.call( arguments, 1 ); // clone arguments array, remove original event from cloned array
 
-            if(delta > 20 && delta < wait){
+            if( delta > 20 && delta < wait ){
                 clearTimeout(tapTimer);
                 return $elem.data('lastTouch', 0).trigger('doubleTap');
             }
